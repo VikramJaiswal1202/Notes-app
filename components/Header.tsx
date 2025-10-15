db1daf41f2b582896e597db534bb4cb402f005ff
Vikram Jaiswal <vikramjaiswal1202@gmail.com>
2025-10-15 21:33:44 +0530

    Darkmode added

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import Image from "next/image"
 import { shadow } from "@/styles/utils"
 import { Button } from "./ui/button"
+import { ModeToggle } from "./darkModeToggle"
 
 const Header = () => {
     const user = null;
@@ -24,12 +25,16 @@ const Header = () => {
                     "Logout"
                 ) : (
                     <>
-                        <Button asChild>
+                        {/* <Button asChild>
                             <Link href="/login">Login</Link>
                         </Button>
+                        <Button asChild variant="outline" className=" hidden sm:block">
+                            <Link href="/sign-up">SignUp</Link>
+                        </Button> */}
                     </>
                 )
                 }
+                <ModeToggle/>
             </div>
         </header>
     )
